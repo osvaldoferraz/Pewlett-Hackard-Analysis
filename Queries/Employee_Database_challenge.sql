d45e3103387afca93baa00ec89cbf382d6715a82
Osvaldo Ferraz <osvaldoferraz@Osvaldos-MacBook-Pro.local>
2020-12-04 00:30:05 -0600

    Chalenge Deliverable 2 complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -31,4 +31,21 @@ FROM unique_titles
 GROUP BY title
 ORDER BY COUNT(title) DESC
 
---
+--Create a Mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) 
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY 
+	e.emp_no ASC;
