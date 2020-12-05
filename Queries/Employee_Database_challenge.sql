0748bb417014441ab0a80d26dab77f2ab260ee91
Osvaldo Ferraz <osvaldoferraz@Osvaldos-MacBook-Pro.local>
2020-12-05 12:07:58 -0600

    Challenge Deliverable 2 updated - complete

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -42,10 +42,12 @@ SELECT DISTINCT ON (e.emp_no)
 	ti.title
 INTO mentorship_eligibility
 FROM employees AS e
-	JOIN dept_emp AS de
+	INNER JOIN dept_emp AS de
 		ON (e.emp_no = de.emp_no)
-	JOIN titles AS ti
+	INNER JOIN titles AS ti
 		ON (e.emp_no = ti.emp_no)
-WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') AND (de.to_date = '9999-01-01')
 ORDER BY 
-	e.emp_no ASC;
+	e.emp_no ASC,
+	ti.to_date DESC
+;
